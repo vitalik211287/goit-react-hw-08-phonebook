ce4e76845371c726a2c4ccc08cf15b86f8112706
vitalik poliakov <vitalik211287@gmail.com>
2022-12-15 22:41:31 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Sections, Title } from './App.styled';
 import Form from './Form/Form';
 import Filter from './Filter/Filter';
