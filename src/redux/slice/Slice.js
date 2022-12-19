d0d1f6a665236efa74cacad24346924cfddc4deb
vitalik poliakov <vitalik211287@gmail.com>
2022-12-19 23:48:59 +0200

    Update Slice.js

--- src/redux/slice/Slice.js
+++ src/redux/slice/Slice.js
@@ -1,5 +1,4 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-// import type { Contacts } from './types'
 
 export const contactApi = createApi({
   providesTags: ['Contacts'],
@@ -43,40 +42,11 @@ export const contactApi = createApi({
   }),
 });
 
-// console.log(contactApi);
+
 export const {
   useGetContactsQuery,
   useAddContactsMutation,
   useDeleteContactsMutation,
 } = contactApi;
 
-// import { createSlice } from '@reduxjs/toolkit';
 
-// const contactList = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-// ];
-
-// const Slice = createSlice({
-//   name: 'contacts',
-//   initialState: { value: contactList, filter: '' },
-//   reducers: {
-//     addContact(state, action) {
-//       state.value.push(action.payload);
-//     },
-//     deleteContact(state, action) {
-//       return {
-//         ...state,
-//         value: state.value.filter(({ id }) => id !== action.payload),
-//       };
-//     },
-//     setFilter(state, action) {
-//       return { ...state, filter: action.payload };
-//     },
-//   },
-// });
-
-// export const { addContact, deleteContact, setFilter } = Slice.actions;
-// export default Slice.reducer;
